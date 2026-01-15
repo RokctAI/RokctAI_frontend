287798135dc2f11398fb861236c18400d65f74e3
Rendani Sinyage <sinyage@gmail.com>
2026-01-15 04:17:45 +0200

    Update decision.ts

--- app/services/all/lending/decision.ts
+++ app/services/all/lending/decision.ts
@@ -2,7 +2,7 @@ import { BaseService, ServiceOptions } from "@/app/services/common/base";
 
 export class DecisionService {
     static async runEngine(applicationId: string, options?: ServiceOptions) {
-        const response = await BaseService.call("rokct.rlending.api.decision.get_credit_score", {
+        const response = await BaseService.call("core.rlending.api.decision.get_credit_score", {
             loan_application: applicationId
         }, options);
         return response?.message;
